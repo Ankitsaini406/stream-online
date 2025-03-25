c7bf52263c56fa795cf89a01c55a1c3a41a846d6
Ankit Saini <as.ankitsaini406@gmail.com>
2025-03-25 11:46:40 +0530

    remove type from MovieCard

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -80,7 +80,7 @@ export default function MoviesPage() {
                 ) : movies.length > 0 ? (
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                         {movies.map((movie) => (
-                            <MovieCard key={movie.id} {...movie} type="movies" />
+                            <MovieCard key={movie.id} {...movie} />
                         ))}
                     </div>
                 ) : (
